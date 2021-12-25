6a63ec8af64f2fc6a6be6c7b6ccaff1087702731
Petik Darya <petikdarya@yandex.ru>
2021-12-25 20:51:30 +0300

    третья попытка

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://trxntssn:zJWzfMOJRShol9kXxiLAqldh0ZSzZ97N@hattie.db.elephantsql.com/trxntssn'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
